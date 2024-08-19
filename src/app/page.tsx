ecccd5eb8e3c4cd7a07a28960e2ab818571361a7
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-19 19:50:00 +0700

    Files upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import FormGenerator from "./form-generator";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <FormGenerator />
+    </main>
+  );
+}
